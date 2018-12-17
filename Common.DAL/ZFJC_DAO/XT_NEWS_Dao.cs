0dcf956c4320c73cb5ac96e6140c4258daa2cafb
jlj_email@163.com <jlj_email@163.com>
2018-12-17 10:01:13 +0800

    增加新闻处理类。

---
+++ Common.DAL/ZFJC_DAO/XT_NEWS_Dao.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using TLKJ.Utils;
+using TLKJ.DB;
+using System.Data;
+
+namespace TLKJ.DAO
+{
+    public class XT_NEWS_Dao : BaseDao<XT_NEWS>
+    {
+        public void init(HttpRequest res, HttpResponse rep)
+        {
+            request = res;
+            response = rep;
+        }
+        public int save(XT_NEWS vo, String cKeyID)
+        {
+            ActiveResult vret = new ActiveResult();
+            List<String> sqls = new List<string>();
+            string sql = null;
+            if (cKeyID.Length == 0)
+            {
+                sql = Insert(vo);
+            }
+            else
+            {
+                sql = Update(vo, "ID='" + cKeyID + "'");
+            }
+            sqls.Add(sql);
+            int iCode= DbManager.ExecSQL(sqls);
+            return iCode;
+
+        }
+        public DBResult Query(String cWhereParm, String cOrderBy, int iPageNo, int iPageSize)
+        {
+            return DbManager.Query("*", "XT_NEWS", cWhereParm, cOrderBy, iPageNo, iPageSize);
+        }
+
+        public int del_item(String cDBKey)
+        {
+            ActiveResult vret = new ActiveResult();
+            string sql = "";
+            sql = "DELETE FROM XT_NEWS WHERE ID='" + cDBKey + "'";
+            int iCode= DbManager.ExecSQL(sql);
+            return iCode;
+        }
+        public int del_list(String cKeyList)
+        {
+            String[] KeyList = cKeyList.Split(',');
+            ActiveResult vret = new ActiveResult();
+            StringBuilder sql = new StringBuilder();
+            sql.Append(" DELETE FROM XT_NEWS ");
+            sql.Append(" WHERE ID IN (");
+            for (int i = 0; i < KeyList.Length; i++)
+            {
+                String cUserID = KeyList[i].Trim();
+                if (i == 0)
+                {
+                    sql.Append("'" + cUserID + "'");
+                }
+                else
+                {
+                    sql.Append(",'" + cUserID + "'");
+                }
+            }
+            sql.Append(" )");
+            return DbManager.ExecSQL(sql.ToString());
+        }
+
+        public XT_NEWS FindOne(string cDBKey)
+        {
+            XT_NEWS vPhone = null;
+            DataTable dtRows = DbManager.QueryData("SELECT * FROM XT_NEWS WHERE ID='" + cDBKey + "'");
+            if ((dtRows != null) && (dtRows.Rows.Count > 0))
+            {
+                vPhone = new XT_NEWS();
+                ReadDB(vPhone, dtRows);
+            }
+            return vPhone;
+        }
+    }
+}
---
+++ Common.DAL/ZFJC_SERVICE/XT_NEWSService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using TLKJ.Utils;
+using TLKJ.DB;
+using System.Data;
+
+namespace TLKJ.DAO
+{
+    public class XT_NEWSService : IService
+    {
+        XT_NEWS_Dao dao = new XT_NEWS_Dao();
+        public void init(HttpRequest res, HttpResponse rep)
+        {
+            request = res;
+            response = rep;
+            readQueryString();
+        }
+
+        public void query()
+        {
+            String cORG_ID = StringEx.getString(request["ORG_ID"]);
+            ActiveResult vret = ActiveResult.Valid(AppConfig.FAILURE);
+
+            String cWhereParm = "ORG_ID like '" + cORG_ID + "%'";
+
+            String cOrderBy = "ORDER BY USER_COUNT ASC";
+            DBResult dbret = dao.Query(cWhereParm, cOrderBy, iPageNo, iPageSize);
+            DataTable dtRows = dbret.dtrows;
+            int iRowsCount = dbret.ROW_COUNT;
+            vret = ActiveResult.Query(dtRows);
+            vret.total = iRowsCount;
+
+            response.Write(vret.toJSONString());
+        }
+
+
+        public void find()
+        {
+            ActiveResult vret = ActiveResult.Valid(AppConfig.FAILURE);
+            String cKeyID = StringEx.getString(request["ID"]);
+            if (String.IsNullOrEmpty(cKeyID))
+            {
+                vret = ActiveResult.Valid("用户账号不能为空！");
+            }
+            else
+            {
+                XT_NEWS vInfo = dao.FindOne(cKeyID);
+                vret = ActiveResult.returnObject(vInfo);
+            }
+            response.Write(vret.toJSONString());
+        }
+
+        public void del_item()
+        {
+            ActiveResult vret = ActiveResult.Valid(AppConfig.FAILURE);
+            String cUSER_COUNT = StringEx.getString(request["dbkey"]);
+            if (String.IsNullOrEmpty(cUSER_COUNT))
+            {
+                vret = ActiveResult.Valid("编码不能为空！");
+            }
+            else
+            {
+                int iCode = dao.del_item(cUSER_COUNT);
+                vret = ActiveResult.Valid(iCode > 0);
+            }
+            response.Write(vret.toJSONString());
+        }
+
+        public void save()
+        {
+            ActiveResult vret = ActiveResult.Valid(AppConfig.SUCCESS);
+            String cKeyID = StringEx.getString(request["ID"]); 
+            String cORG_ID = StringEx.getString(request["org_id"]);
+            String cCLS_ID = StringEx.getString(request["cls_id"]);
+
+            XT_NEWS vo = new XT_NEWS();
+            vo = (XT_NEWS)RequestUtil.readFromRequest(request, vo);
+            vo.cls_id = cCLS_ID;
+            vo.update_time = StringEx.getString(DateUtils.getDayTimeNum());
+            if (String.IsNullOrEmpty(cORG_ID))
+            {
+                vret = ActiveResult.Valid("单位编码不能为空！");
+            }
+            else if (String.IsNullOrEmpty(cCLS_ID))
+            {
+                vret = ActiveResult.Valid("类型不能为空！");
+            } 
+
+            if (vret.result == AppConfig.SUCCESS)
+            {
+                int iCode = dao.save(vo, cKeyID);
+                vret = ActiveResult.Valid(iCode);
+            }
+            response.Write(vret.toJSONString());
+        }
+    }
+}
